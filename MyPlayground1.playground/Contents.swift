2b7393b8f76a7841c5a7240e5542f4c5e1ec6847
jamariod <j25ghs99@aol.com>
2020-05-16 21:53:56 -0400

    Add files via upload

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,131 @@
+import UIKit
+
+////Most Common Swift Data Types
+//
+////String: Piece of text
+////Int: Whole numbers positive and negative
+////Float: Decimal numbers
+////Double: Large decimal numbers
+////Bool: True or false
+//
+//var str = "Hello, playground"
+//
+//var firstName = "Jamario"
+//var newName = "Davis" + " " + firstName
+//print(newName)
+//
+//var stockPrice = 100
+//print(stockPrice)
+//
+//stockPrice = 50
+//print(stockPrice)
+//
+//let middleName = "Antwan"
+//print(middleName)
+//
+//
+//
+////Type annotation
+////A type annotation explicitly specifies the type of a variable or expression
+//
+//
+//
+//var personName:String = "Jim Kelly"
+//var personAge:Int = 37
+//var aBool:Bool = true
+//
+///*
+// The big difference between the two is that Float
+// variables can only hold seven digits of precision while
+// Double variables can hold twice that number.
+// */
+//var highScore:Float = 100.1234567
+//var piNumber:Double = 3.14159265359
+
+
+//IF STATEMENTS
+
+//let a = 10
+//
+//if a <= 10 {
+//    print("a is less that 10")
+//}
+
+//ELSE IF
+
+//let a = 100
+//if a <= 10 {
+//    print("a is less than 10")
+//}
+//else if a > 15 {
+//    print(" a is greater than 15")
+//}
+
+//SWITCH STATEMENTS
+
+//let sportsTeam = 1
+//
+//switch sportsTeam {
+//case 0:
+//    print(" I am a Jets")
+//case 1:
+//    print("I am a Bills fan")
+//case 2:
+//    print("I am a Packers fan")
+//case 3:
+//    print("I am a Saints fan")
+//default:
+//    print("I don't have a team")
+//}
+
+
+//Loops
+
+//For In Loop
+
+//for counter in 1...5 {
+//    print(counter)
+//}
+
+// Another way to write For In Loop
+
+//for _ in 1...5 {
+//    print("Hello World")
+//}
+
+//let count = 1...10
+//for number in count {
+//    print("Number in count \(number)")
+//}
+
+//let albums = ["The Blueprint", "College Dropout", "Stillmatic"]
+//
+//for album in albums {
+//    print("\(album) is on Apple Music")
+//}
+//for i in 0..<5 {
+//    print(i)
+//}
+
+//var names = ["Jake", "Sam", "John"]
+//
+//for name in names {
+//    print(name)
+//}
+//
+//let numbers = [1, 2, 4, 5, 9, 23]
+//
+//for e in numbers {
+//    print(e)
+//}
+
+
+
+//var i = 0
+//while i < 6 {
+//    i += 1
+//    print(i)
+//}
+
+
+
